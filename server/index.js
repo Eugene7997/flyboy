580b093450ede898ab1888f0eebf957bb7c67223
john <ypoh007@e.ntu.edu.sg>
2024-08-08 16:33:07 +0800

    initial server hello world testing

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const express = require("express")
+var Mongoclient = require("mongodb").Mongoclient
+const cors = require("cors");
+
+const app = express()
+app.use(cors())
+
+app.get("/", (req, res)=>{
+    res.send("App is Working");
+})
+
+app.listen(5000);
