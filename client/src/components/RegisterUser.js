4763799966e607f0e9ccec91c72ef40317ccb7b1
john <ypoh007@e.ntu.edu.sg>
2024-08-08 19:32:35 +0800

    added toggle hidden password feature

--- client/src/components/RegisterUser.js
+++ client/src/components/RegisterUser.js
@@ -1,6 +1,21 @@
 import { Form } from "react-bootstrap";
+import { useState } from "react";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import IconButton from '@mui/material/IconButton';
 
 function UserRegistrationForm() {
+    const [values, setValues] = useState({
+        password: "",
+        showPassword: false
+    });
+    
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
     const submitForm = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
@@ -17,8 +32,13 @@ function UserRegistrationForm() {
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="text" name="password" placeholder="Enter Password" />
+                    <Form.Control type={values.showPassword? "text": "password"} name="password" placeholder="Enter Password" />
                 </Form.Group>
+                <IconButton 
+                    onClick={handleClickShowPassword}
+                >
+                    {values.showPassword? <VisibilityIcon/>: <VisibilityOffIcon/>}
+                </IconButton>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </>
