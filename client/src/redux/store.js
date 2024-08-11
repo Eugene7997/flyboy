2abc120e2525a31a48879b6fa80c734cce35966b
john <ypoh007@e.ntu.edu.sg>
2024-08-11 14:13:23 +0800

    added flightSlice for redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import flightReducer from './flight/flightSlice';
 import {persistReducer, persistStore} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'
 
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    flight: flightReducer
 })
 
 const persistConfig = {
