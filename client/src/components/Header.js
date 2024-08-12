97ee74038fc9f8faf90830c452b9acde66f191ac
john <ypoh007@e.ntu.edu.sg>
2024-08-12 19:08:00 +0800

    beautified the web page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,45 +1,49 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
-    const { currentUser } = useSelector((state) => state.user)
+    const { currentUser } = useSelector((state) => state.user);
+    
     return (
-        <div className="bg-slate-200">
-            <div className='flex justify-between items-center max-w-6-xl mx-auto p-3'>
-                <Link to='/'>
-                    <h1 className="font-bold">FlyBoy</h1>
+        <header className="bg-gray-100 shadow-md">
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+                <Link to='/' className="text-2xl font-semibold text-black-600 hover:text-blue-800">
+                    <div className='flex flex-items'>
+                        <img src="/images/flyboy.png" alt="FlyBoy Logo" className="w-8 h-8 mr-2" />
+                        FlyBoy
+                    </div>
                 </Link>
-                <ul className='flex gap-4'>
-                    <Link to='/'>
-                        <li>Home</li>
-                    </Link>
-                    <Link to='/about'>
-                        <li>About</li>
-                    </Link>
-                    {currentUser ? (
-                        <>
-                            <Link to='/flightlogs'>
-                                <li>Flights</li>
-                            </Link>
-                            <Link to='/profile'>
-                                <li>Profile</li>
-                            </Link>
-                        </>
-                    ) : (
-                        <Link to='/sign_in'>
-                            <li>Sign In</li>
-                        </Link>
-                    )}
-                    {
-                        !currentUser && (
-                            <Link to='/sign_up'>
-                                <li>Sign Up</li>
-                            </Link>
-                        )
-                    }
-                </ul>
+                <nav>
+                    <ul className='flex gap-6'>
+                        <li>
+                            <Link to='/' className="text-gray-800 hover:text-gray-900">Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/about' className="text-gray-800 hover:text-gray-900">About</Link>
+                        </li>
+                        {currentUser ? (
+                            <>
+                                <li>
+                                    <Link to='/flightlogs' className="text-gray-800 hover:text-gray-900">Flights</Link>
+                                </li>
+                                <li>
+                                    <Link to='/profile' className="text-gray-800 hover:text-gray-900">Profile</Link>
+                                </li>
+                            </>
+                        ) : (
+                            <li>
+                                <Link to='/sign_in' className="text-gray-800 hover:text-gray-900">Sign In</Link>
+                            </li>
+                        )}
+                        {!currentUser && (
+                            <li>
+                                <Link to='/sign_up' className="text-gray-800 hover:text-gray-900">Sign Up</Link>
+                            </li>
+                        )}
+                    </ul>
+                </nav>
             </div>
-        </div>
-    )
+        </header>
+    );
 }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { 
+import {
     signOut,
     deleteUserFailure,
     deleteUserStart,
-    deleteUserSuccess 
+    deleteUserSuccess
 } from '../redux/user/userSlice'
-
 
 export default function Profile() {
     const { currentUser } = useSelector(state => state.user)
@@ -22,31 +21,46 @@ export default function Profile() {
     }
 
     const handleDeleteUser = async () => {
-        try {
-            dispatch(deleteUserStart())
-            console.log(currentUser._id)
-            const res = await fetch(`http://localhost:5000/api/users/delete/${currentUser._id}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                credentials: 'include',
-            })
-            const data = await res.json()
-            dispatch(deleteUserSuccess(data))
-        }
-        catch (err) {
-            dispatch(deleteUserFailure(err.message))
-            console.error(err)
+        if (window.confirm('Are you sure you want to delete your account? This action cannot be undone.')) {
+            try {
+                dispatch(deleteUserStart())
+                const res = await fetch(`http://localhost:5000/api/users/delete/${currentUser._id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'include',
+                })
+                const data = await res.json()
+                dispatch(deleteUserSuccess(data))
+            }
+            catch (err) {
+                dispatch(deleteUserFailure(err.message))
+                console.error(err)
+            }
         }
     }
+
     return (
-        <div>
-            <h1>Welcome {currentUser?.username}</h1>
-            <div className='flex gap-2 mt-5'>
-                <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete user</span>
-                <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
+        <div className="flex items-center justify-start h-screen p-6 bg-gray-100 bg-cover bg-center" style={{ backgroundImage: 'url(/images/wallhaven-g8yqk7.jpg)' }}>
+            <div className="p-8 max-w-md bg-gray-800 bg-opacity-80 rounded-lg shadow-lg">
+                <h1 className="text-3xl text-white font-bold text-center mb-6">Welcome, {currentUser?.username}</h1>
+                <div className="flex flex-col gap-4 mt-6">
+                    <button
+                        onClick={handleDeleteUser}
+                        className="bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+                    >
+                        Delete User
+                    </button>
+                    <button
+                        onClick={handleSignOut}
+                        className="bg-gray-600 text-white py-2 px-4 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                    >
+                        Sign Out
+                    </button>
+                </div>
             </div>
         </div>
     )
+       
 }
